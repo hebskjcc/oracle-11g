64f919a48f6e4bd68c042c90e3f7ef6b0351b58f
hebskjcc <43854598+hebskjcc@users.noreply.github.com>
2018-12-28 15:38:46 +0800

    Update entrypoint.sh

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -9,6 +9,14 @@ if [ ! -d "/opt/oracle/app/product/11.2.0/dbhome_1" ]; then
 	echo_yellow "Database is not installed. Installing..."
 	/assets/install.sh
 fi
-
+	mkdir -p -m 755 /opt/oracle/app
+	mkdir -p -m 755 /opt/oracle/oraInventory
+	mkdir -p -m 755 /opt/oracle/dpdump
+	chown -R oracle:oinstall /opt/oracle
+	cat /assets/profile >> ~oracle/.bash_profile
+	cat /assets/profile >> ~oracle/.bashrc
+	
+	cat /assets/profile >> ~root/.bash_profile
+	cat /assets/profile >> ~root/.bashrc
 su oracle -c "/assets/entrypoint_oracle.sh"
 
